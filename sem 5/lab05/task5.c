94bd35ff9c38e030adc4d9d843b232d2f40af592
Danya <daniilmokeev13@gmail.com>
2020-11-18 15:47:54 +0300

    haha

---
+++ sem 5/lab05/task5.c
@@ -0,0 +1,88 @@
+/*
+DIY sig handler
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+#define MESSAGE_SIZE 32
+
+void mySignalHandler(int snum){
+	printf("\n\nHandlig signal snum = %d in prosses...\n", snum);
+	printf("Done!\n\n");
+}
+
+int main(){
+	int child_1, child_2;
+	signal(SIGINT, mySignalHandler);
+	
+	//initializing pipe
+	int fd[2];
+	if (pipe(fd) == -1){
+		printf("Coundn't create a pipe\n");
+		exit(1);
+	}
+	
+	child_1 = fork();
+	if(child_1 == -1){
+		perror("Coulnd't fork child #1");
+		exit(1);
+	}
+	if (child_1 == 0){
+		close(fd[0]);
+		if (write(fd[1], "Hello from child #1, parent!\n", MESSAGE_SIZE) > 0)
+			printf("Child #1 sent a greeting to the parent\n");
+		 return 0;
+	}
+	if (child_1 > 0){
+		child_2 = fork();
+		if(child_2 == -1){
+			perror("Coulnd't fork child #2");
+			exit(1);
+	        }
+	        if (child_2 == 0){
+	        	close(fd[0]);
+		if (write(fd[1], "Hello from child #2, parent!\n", MESSAGE_SIZE) > 0)
+			printf("Child #2 sent a greeting to the parent\n");
+		 	return 0;
+	        }else{
+	        	printf("\nParent's waiting for Ctrl+C being pressed to read messages from children\n");
+		 	pause();
+		  	
+		  	char msg1[MESSAGE_SIZE], msg2[MESSAGE_SIZE];
+			close(fd[1]);
+			read(fd[0], msg1, MESSAGE_SIZE);
+			read(fd[0], msg2, MESSAGE_SIZE);
+			
+			printf("Parent read this from his children: \n%s\n%s", msg1, msg2); 
+			
+		  	pid_t child_pid;
+		 	int status;
+		 			
+		 	//waiting for the second child to finish
+		 	child_pid = wait(&status);
+		 	if (WIFEXITED(status))
+		 		printf("\nParent: child with pid = %d finished with code %d\n",
+		 		 child_pid, WEXITSTATUS(status) );
+		 	else if (WIFSTOPPED(status))
+		 		printf("\nParent: child %d finished with code %d\n",
+		 		 child_pid, WSTOPSIG(status) );
+		 		 
+		 	//waiting for the first child to finish	 
+		 	child_pid = wait(&status);
+		 	if (WIFEXITED(status))
+		 		printf("\nParent: child with pid = %d finished with code %d\n",
+		 		 child_pid, WEXITSTATUS(status) );
+		 	else if (WIFSTOPPED(status))
+		 		printf("\nParent: child %d finished with code %d\n",
+		 		 child_pid, WSTOPSIG(status) );
+		 
+		 return 0;
+	        }
+	}
+	
+	
+}
+
+
