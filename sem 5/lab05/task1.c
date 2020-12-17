9280405cb21f8d70b7ddc33665856e54623ad00d
MokeevDan <Mokeev.Danil@wildberries.com>
2020-12-17 16:46:22 +0300

    task1

--- sem 5/lab05/task1.c
+++ sem 5/lab05/task1.c
@@ -1,144 +1,179 @@
-#include <sys/sem.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <sys/shm.h>
+#include <signal.h>
 #include <stdio.h>
-#include <sys/ipc.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-#define BUFFER_SIZE 10 
+#define SEM_BIN   0
+#define SEM_EMPTY 1
+#define SEM_FULL  2
+
 #define P -1
 #define V 1
 
-#define SE 0
-#define SF 1
-#define SB 2
+#define PRODUCERS_COUNT 3
+#define CONSUMERS_COUNT 3
 
-char* shared_buffer;
-int* pos_prod;
-int* pos_cons;
-int num = 0;
+#define N 20
 
-int semid = 1;
+#define PERMS S_IRWXU | S_IRWXG | S_IRWXO //permition to read, write & execute by user, group & others
 
-const int PERMISSIONS = S_IRWXU | S_IRWXG | S_IRWXO; //permition to read, write & execute by user, group & others
-const int amount_of_consumers = 3; //amount of consumers
-const int amount_of_producers = 3; //amount of producers
+int sem_id = -1;
+int shm_id = -1;
 
-struct sembuf sembuf_producer_start[2] = {{SB, P, 0}, {SE, V, 0}}; 
-struct sembuf sembuf_producer_stop[2] = {{SF, P, 0}, {SB, P, 0}};
-struct sembuf sembuf_consumer_start[2] = {{SB, P, 0}, {SF, V, 0}};
-struct sembuf sembuf_consumer_stop[2] = {{SE, P, 0}, {SB, P, 0}};
+int *shm = NULL;
+int *shm_prod = NULL;
+int *shm_cons = NULL;
 
-void produce(const int number){
-	while(1){
-		sleep(rand() % 2);
-		
-		if (semop(semid, sembuf_producer_start, 2) == -1){
-			perror("Error semop producer start");
-			exit(1);
-		}
-		
-		printf("Producer %d produces %d \n", getpid(), *pos_prod);
-		*(shared_buffer + *pos_prod) = *pos_prod;
-		(*pos_prod)++;
-		
-		if (semop(semid, sembuf_producer_stop, 2) == -1){
-			perror("Error semop producer stop");
-			exit(1);
-		}
-	}
-	
+struct sembuf producer_p[2] = {
+    {SEM_EMPTY, P, SEM_UNDO},
+    {SEM_BIN,   P, SEM_UNDO}
+};
+struct sembuf producer_v[2] = {
+    {SEM_BIN,   V, SEM_UNDO},
+    {SEM_FULL,  V, SEM_UNDO}
+};
+
+struct sembuf consumer_p[2] = {
+    {SEM_FULL,  P, SEM_UNDO},
+    {SEM_BIN,   P, SEM_UNDO}
+};
+struct sembuf consumer_v[2] = {
+    {SEM_BIN,   V, SEM_UNDO},
+    {SEM_EMPTY, V, SEM_UNDO}
+};
+
+void fork_children(const int n, void (*func)(const int)) {
+    for (int i = 0; i < n; ++i) {
+        const pid_t pid = fork();
+
+        if (pid == -1) {
+            perror("fork");
+            exit(1);
+        } else if (pid == 0) {
+            if (func) {
+                func(i);
+            }
+            exit(1);
+        }
+    }
 }
 
-void consume(const int number){
-	while(1){
-		sleep(rand() % 2);
-		
-		if (semop(semid, sembuf_consumer_start, 2) == -1){
-			perror("Error semop consumer start");
-			exit(1);
-		}
-		
-		printf("Consumer %d produces %d \n", getpid(), *pos_prod);
-		*(shared_buffer + *pos_cons) = *pos_cons;
-		(*pos_cons)++;
-		
-		if (semop(semid, sembuf_consumer_stop, 2) == -1){
-			perror("Error semop consumer stop");
-			exit(1);
-		}
-	}
+void wait_children(const int n) {
+    for (int i = 0; i < n; ++i) {
+        int status;
+        const pid_t child_pid = wait(&status);
+        if (child_pid == -1) {
+            perror("wait");
+            exit(1);
+        }
+
+        if (WIFEXITED(status)) {
+            printf("Process %d returns %d\n", child_pid, WEXITSTATUS(status));
+        } else if (WIFSIGNALED(status)) {
+            printf("Process %d terminated with signal %d\n", child_pid, WTERMSIG(status));
+        } else if (WIFSTOPPED(status)) {
+            printf("Process %d stopped due signal %d\n", child_pid, WSTOPSIG(status));
+        }
+    }
 }
 
-void fork_children(const int numberOfChildren, void(*func)(const int)){
-	for (int i = 0; i < numberOfChildren; ++i){
-		const int pid = fork();
-		
-		if (pid == -1){
-			perror(1);
-			exit(1);
-		}else if(pid == 0) {
-			func(i); 
-		}
-		exit(1);
-	}
+void producer(const int id) {
+    for (char i = 97; i<= 122; ++i) {
+        sleep(rand() % 2);
+        if (semop(sem_id, producer_p, 2) == -1) {
+            perror("semop");
+            exit(1);
+        }
+        // write next value in shared memory
+        *(shm + *shm_prod) = i;
+        printf("Producer %d (pid %d) produces %c\n", id, getpid(), i);
+        (*shm_prod)++;
+
+        if (semop(sem_id, producer_v, 2) == -1) {
+            perror("semop");
+            exit(1);
+        }
+    }
 }
 
-void wait_children(const int numberOfChildren){
-	for (int i = 0; i<numberOfChildren; ++i){
-		int status;
-		int child_pid = wait(&status);
-		if (child_pid == -1){
-			perror("Error child_pid");
-			exit(1);
-		}
-		
-		if (WIFEXITED(status)){
-			printf("Procces %d returned %s status", child_pid, WEXITSTATUS(status));
-		}
-	}
+void consumer(const int id) {
+    while(1) {
+        sleep(rand() % 2);
+        if (semop(sem_id, consumer_p, 2) == -1) {
+            perror("semop");
+            exit(1);
+        }
+
+        printf("Consumer %d (pid %d) consumes %c\n", id, getpid(), *(shm + *shm_cons));
+		if (*(shm + *shm_cons) == 122)
+			return;
+        (*shm_cons)++;
+
+        if (semop(sem_id, consumer_v, 2) == -1) {
+            perror("semop");
+            exit(1);
+        }
+    }
 }
 
-int main(){
-	int shmid, semid;
-	
-	printf("Parentpid %d\n", getpid());
-	
-	//creating shared memory area with size not less then our shared buffer
-	shmid = shmget(IPC_PRIVATE, BUFFER_SIZE * sizeof(char), IPC_CREAT | PERMISSIONS);
-	if (shmid == -1){
-		perror("Coulderrornt create shared mem seg\n");
-		exit(1);
-	}
-	//attaching shared memory onto our address space
-	char* addr = shmat(semid, NULL, 0);
-	if (addr == (void *) -1){
-		perror("Couldnt attach shm\n");
-		exit(1);
-	}
-	
-	semid = semget(IPC_PRIVATE, 3, IPC_CREAT | PERMISSIONS);
-	if (semid == -1){
-		perror("An error occured while creating semathors\n");
-		exit(1);
-	}
-	
-	if (
-		semctl(semid, SE, SETVAL, BUFFER_SIZE) == -1 ||
-		semctl(semid, SF, SETVAL, 0) == -1 ||
-		semctl(semid, SB, SETVAL, 1) == -1 )
-	{ 
-		perror("An error occured while semctl\n");
-		exit(1);
-	}
-		
-	fork_children(amount_of_producers, produce);
-	fork_children(amount_of_consumers, consume);
-	
-	wait_children(amount_of_producers+amount_of_consumers);
-	
-	return 0;
+void sigint_handler(const int sig) {
+    if (sig == SIGINT) {
+        printf("\nSIGINT signal was handled\n");
+    }
+}
+
+void init_semaphores() {
+    sem_id = semget(IPC_PRIVATE, 3, IPC_CREAT | PERMS);
+    if (sem_id == -1) {
+        perror("semget");
+        exit(1);
+    }
+    if (semctl(sem_id, SEM_BIN,   SETVAL, 1) == -1 ||
+        semctl(sem_id, SEM_EMPTY, SETVAL, N) == -1 ||
+        semctl(sem_id, SEM_FULL,  SETVAL, 0) == -1) {
+        perror("semctl");
+        exit(1);
+    }
+}
+
+void init_shared_memory() {
+    shm_id = shmget(IPC_PRIVATE, (N + 3) * sizeof(int), IPC_CREAT | PERMS);
+    if (shm_id == -1) {
+        perror("shmget");
+        exit(1);
+    }
+    shm = shmat(shm_id, 0, 0);
+    if (shm == (void *) -1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    shm_prod = shm;
+    shm_cons = shm + 1;
+    *shm_prod = 0;
+    *shm_cons = 0;
+    shm = shm + 2;
+}
+
+int main() {
+    int children = 0;
+    srand((unsigned int) time(NULL));
+
+    init_semaphores();
+    init_shared_memory();
+
+    fork_children(PRODUCERS_COUNT, producer);
+    fork_children(CONSUMERS_COUNT, consumer);
+
+    signal(SIGINT, sigint_handler);
+
+    wait_children(PRODUCERS_COUNT + CONSUMERS_COUNT);
+
+    shmctl(shm_id, IPC_RMID, NULL);
+    semctl(sem_id, SEM_BIN, IPC_RMID, 0);
 }
